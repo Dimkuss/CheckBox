c067c0b6646da0791060ad479b026d7529335914
Mokhov Dmitriy <mohow-95@mail.ru>
2020-12-20 18:31:49 +0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText inputMoney;
+    private EditText inputInfo;
+    private Button BtnOK;
+    private CheckBox CardBox;
+    private CheckBox PhoneBox;
+    private CheckBox CashBox;
+    private String mInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        inputMoney = findViewById(R.id.inputMoney);
+        inputInfo = findViewById(R.id.info);
+        BtnOK = findViewById(R.id.buttonOK);
+        CardBox = findViewById(R.id.checkBoxCard);
+        PhoneBox = findViewById(R.id.checkBoxPhone);
+        CashBox = findViewById(R.id.checkBoxCash);
+        CardBox.setOnCheckedChangeListener(checkedChangeListener);
+        PhoneBox.setOnCheckedChangeListener(checkedChangeListener);
+        CashBox.setOnCheckedChangeListener(checkedChangeListener);
+        BtnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Вы выбрали оплату на сумму " + inputMoney.getText().toString() + " Путём " + mInfo, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void resetCheckBoxes() {
+        CardBox.setChecked(false);
+        PhoneBox.setChecked(false);
+        CashBox.setChecked(false);
+    }
+
+    CompoundButton.OnCheckedChangeListener checkedChangeListener = new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            if (b) {
+                switch (compoundButton.getId()) {
+                    case R.id.checkBoxCard:
+                        resetCheckBoxes();
+                        CardBox.setChecked(true);
+                        inputInfo.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        mInfo = "Банковского перевода";
+                        break;
+                    case R.id.checkBoxPhone:
+                        resetCheckBoxes();
+                        PhoneBox.setChecked(true);
+                        inputInfo.setInputType(InputType.TYPE_CLASS_PHONE);
+                        mInfo = "Оплаты по телефону";
+                        break;
+                    case R.id.checkBoxCash:
+                        resetCheckBoxes();
+                        CashBox.setChecked(true);
+                        inputInfo.setInputType(InputType.TYPE_CLASS_TEXT);
+                        mInfo = "Наличного расчета курьеру";
+                        break;
+                    default:
+                }
+            }
+        }
+
+    };
+
+}
